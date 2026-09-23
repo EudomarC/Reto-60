30b1ab53e6851d2c8fa05a8db768e2a324fef8ff
EudomarC <eudomarcolls8@gmail.com>
2026-09-23 12:44:50 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    q: "¿Cuál es el planeta más grande del sistema solar?",
+    a: ["Marte", "Júpiter", "Saturno", "Venus"],
+    c: 1
+  },
+  {
+    q: "¿Cuánto es 12 × 8?",
+    a: ["86", "96", "106", "88"],
+    c: 1
+  },
+  {
+    q: "¿Cuál es la capital de Argentina?",
+    a: ["Córdoba", "Buenos Aires", "Mendoza", "Rosario"],
+    c: 1
+  }
+];
